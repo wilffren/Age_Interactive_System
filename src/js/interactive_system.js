3ef19e42a144347375a153898f96396280b32742
Wilffren Efrain Muñoz Velasquez <wilffren14@gmail.com>
2025-06-19 15:25:54 -0500

    Feat: The main function was created

--- src/js/interactive_system.js
+++ src/js/interactive_system.js
@@ -1,6 +1,21 @@
+function validation(){
+
 const name = document.getElementById("name").value
 const age = document.getElementById("age").value
 const btnValidation = document.getElementById("btn-validation")
 
-const result = document.getElementById("result")
-result.textContent = age
+const result = document.getElementById("result").innerHTML = btnValidation
+
+
+let message
+
+
+if (isNaN(age)){
+    message = "Error: Please enter a valid age in numbers"
+}else if (age < 18){
+    message = "Hello " + name + ", You're a minor. Keep learning and enjoying coding!"
+}else{
+    message = "Hello " + name + ", You're of legal age. Get ready for great opportunities in the world of programming!"
+}
+
+}

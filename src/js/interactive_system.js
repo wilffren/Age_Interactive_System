f5a68c1568a9b86039dc941715c177c890561d46
Wilffren Efrain Muñoz Velasquez <wilffren14@gmail.com>
2025-06-19 17:35:19 -0500

    Feat: The comments was created

--- src/js/interactive_system.js
+++ src/js/interactive_system.js
@@ -1,21 +1,23 @@
+/* The principal variables for button and section message was created */
 let btnValidation = document.getElementById("btn-validation")
 let result = document.getElementById("result")
 
+/*Button with function click, was created */
 btnValidation.addEventListener("click", function () {
     let name = document.getElementById("name").value
     let age = document.getElementById("age").value
-
+//Message variable for the distint messages depend of condition.
     let message
 
-
-    if (isNaN(age) || age <= 0) {
+//Conditional for validation of age
+    if (isNaN(age) || age <= 0) { //isNaN for isn't a number or age invalid
         message = "Error: Please enter a valid age in numbers"
     } else if (age < 18) {
         message = "Hello " + name + ", You're a minor. Keep learning and enjoying coding!"
     } else {
         message = "Hello " + name + ", You're of legal age. Get ready for great opportunities in the world of programming!"
     }
-
+//Finally, show the answer in the variable result
     result.textContent = message
 })
 

a09ed76e9b2ac632c9e1fccf53415db98998f133
Wilffren Efrain Muñoz Velasquez <wilffren14@gmail.com>
2025-06-19 15:12:57 -0500

    Feat: structure and logic was created

---
+++ src/js/interactive_system.js
@@ -0,0 +1,6 @@
+const name = document.getElementById("name").value
+const age = document.getElementById("age").value
+const btnValidation = document.getElementById("btn-validation")
+
+const result = document.getElementById("result")
+result.textContent = age
